5fbdb137b8a38ca552e62c618f4b0efaa5a23946
giankocr <gian@gianko.com>
2017-07-07 10:10:38 -0600

    Actualizacion de contenido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { SkilsComponent } from './skils/skils.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    SkilsComponent,
+    SkillsComponent,
+    EducationComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
